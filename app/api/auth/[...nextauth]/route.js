014661885381d5bf35b518114ab4852543ba3336
TommyTheMob <zv3rbrus@gmail.com>
2024-01-31 14:36:36 +0300

    Realize registration through Google OAuth, create-prompt api and page for it

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,37 +11,39 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-
-        session.user.id = sessionUser._id.toString()
-
-        return session
-    },
-    async signIn({ profile }) {
-        try {
-            await connectToDB()
-
-            // check if a user is already exists
-            const userExists = await User.findOne({
-                email: profile.email
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
             })
 
-            // if not, create a new user
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(' ', '').toLowerCase(),
-                    image: profile.picture
-                })
-            }
+            session.user.id = sessionUser._id.toString()
 
-            return true
-        } catch (e) {
-            console.log(e)
-            return false
+            return session
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB()
+
+                // check if a user is already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                })
+
+                // if not, create a new user
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(' ', '').toLowerCase(),
+                        image: profile?.picture
+                    })
+                }
+
+                return true
+            } catch (e) {
+                console.log(e)
+                return false
+            }
         }
     }
 })
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import {useState, useEffect} from "react";
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
-    const isUserLoggedIn = true
+    const { data: session } = useSession()
 
     const [providers, setProviders] = useState(null)
     const [toggleDropdown, setToggleDropdown] = useState(false)
@@ -36,7 +36,7 @@ const Nav = () => {
 
             {/*Desktop Navigation*/}
             <div className='sm:flex hidden'>
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className='flex gap-3 md:gap-5'>
                         <Link
                             href='/create-prompt'
@@ -55,7 +55,7 @@ const Nav = () => {
 
                         <Link href='/profile'>
                             <Image
-                                src='/assets/images/logo.svg'
+                                src={session?.user.image}
                                 alt='profile'
                                 width={37}
                                 height={37}
@@ -83,10 +83,10 @@ const Nav = () => {
 
             {/*Mobile Navigation*/}
             <div className='sm:hidden flex relative'>
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className='flex'>
                         <Image
-                            src='/assets/images/logo.svg'
+                            src={session?.user.image}
                             alt='profile'
                             width={37}
                             height={37}
