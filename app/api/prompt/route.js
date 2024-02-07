2439747d5314f5a1ec3f3eede37bef3c7d6638d6
TommyTheMob <zv3rbrus@gmail.com>
2024-02-07 15:50:02 +0300

    remove revalidate for fetching posts in feed and your profile page; instead add force dynamic prompt/route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,6 +2,8 @@ import {connectToDB} from "@/utils/database";
 
 import Prompt from '@/models/prompt'
 
+export const dynamic = 'force-dynamic'
+
 export const GET = async (req) => {
     try {
         await connectToDB()
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -14,7 +14,7 @@ const MyProfile = () => {
 
     useEffect(() => {
         const fetchPosts = async () => {
-            const response = await fetch(`/api/users/${session?.user.id}/posts`, {next: {revalidate: 10}})
+            const response = await fetch(`/api/users/${session?.user.id}/posts`)
             const data = await response.json()
 
             setPosts(data)
