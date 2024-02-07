6e25c7f55461fc6ec7fd121502099c2c54e6ab66
TommyTheMob <zv3rbrus@gmail.com>
2024-02-07 15:25:15 +0300

    add revalidate for fetching posts in feed and your profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -14,7 +14,7 @@ const MyProfile = () => {
 
     useEffect(() => {
         const fetchPosts = async () => {
-            const response = await fetch(`/api/users/${session?.user.id}/posts`)
+            const response = await fetch(`/api/users/${session?.user.id}/posts`, {next: {revalidate: 10}})
             const data = await response.json()
 
             setPosts(data)
